e613061a62bc082f15eae020b14c118f60617fc1
crispulum <crispulum@gmail.com>
2023-07-20 20:46:45 -0500

    added game, deck functionality

---
+++ main.js
@@ -0,0 +1,71 @@
+//blackjack functionality.
+
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
+
+
+
+//player class
+
+//game class
+class Game {
+    constructor() {
+        this.deck = new Deck;
+    }
+
+
+}
+
+
+//deck
+class Deck {
+
+
+
+    constructor() {
+
+        this.deckOrder = [
+            '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH',
+            '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC',
+            '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD',
+            '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS',]
+        this.deckOrder = shuffle(this.deckOrder);
+
+
+    }
+
+
+
+
+}
+
+
+let testDeck = new Deck()
+console.log(testDeck.deckOrder)
+//dealer class
+
+//hit function
+
+//player done w hand function
+
+//check value w hand function
+
+
+//cards
+
+//new game
